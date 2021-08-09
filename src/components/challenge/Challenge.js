2dc282f9a5504d2d11dfa466baf7fbbd64fe1f61
Rochelle Edwards <rochellelynn.programmer@gmail.com>
2021-08-08 22:37:57 -0500

    finalized most of the styling, got modal pop up functioning, pushing to check speed test again

--- src/components/challenge/Challenge.js
+++ src/components/challenge/Challenge.js
@@ -1,9 +1,17 @@
-import React from "react";
+import { useState } from "react";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 import { Typography, Paper } from "@material-ui/core";
-import FormatQuoteIcon from "@material-ui/icons/FormatQuote";
+import Button from "@material-ui/core/Button";
 import { ListComponent, WadeList } from "./List";
+import CloudDownloadIcon from "@material-ui/icons/CloudDownload";
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Link from "@material-ui/core/Link";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -22,15 +30,47 @@ const useStyles = makeStyles((theme) => ({
     height: "100%",
     width: "100%",
   },
+  wadeImageContain: {
+    width: "70%",
+  },
+  wadeImage: {
+    height: "100%",
+    width: "100%",
+  },
+  quotes: {
+    margin: ".5rem",
+  },
+  important: {
+    backgroundColor: "#ECD662",
+    padding: "3%",
+  },
+  centerMe: {
+    textAlign: "center",
+  },
+  button: {
+    backgroundColor: "#FF7600",
+  },
 }));
 const Challenge = () => {
   const styles = useStyles();
+
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <div className={styles.root}>
       <Grid container spacing={3}>
         <Grid item xs={12}>
-          <Typography variant="h3">Get Access To The AWESOME Health Course</Typography>
+          <Typography variant="h3" className={styles.centerMe} style={{ color: "red" }}>
+            Get Access To The AWESOME Health Course
+          </Typography>
         </Grid>
         <Grid item container sm={12} md={6}>
           <Grid item xs={12}>
@@ -44,7 +84,7 @@ const Challenge = () => {
             <ListComponent />
           </Grid>
           <Grid item xs={12}>
-            <Paper>
+            <Paper className={styles.important}>
               <p>
                 IMPORTANT: Click the orange button for access. After you enter your information, please check your email
                 for setting up your account to login to the private program platform.
@@ -58,40 +98,84 @@ const Challenge = () => {
             <p>Wade Lightheart</p>
           </Grid>
         </Grid>
-        <Grid item xs={12} md={6}>
+        <Grid item xs={12} md={6} className={styles.centerMe}>
           <div classnames={styles.imageContain}>
             <img
               className={styles.challengeImage}
               src={`${process.env.PUBLIC_URL}/assets/AwesomeHealthCourse-Banner_555x500.jpeg`}
-              alt="logo"
+              alt="health-course-banner"
             />
           </div>
+          <Button
+            variant="contained"
+            className={styles.button}
+            size="large"
+            endIcon={<CloudDownloadIcon />}
+            onClick={handleClickOpen}
+          >
+            DOUBLE MY ENERGY
+          </Button>
+          <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+            <DialogTitle id="form-dialog-title">YES, I WANT TO ENHANCE MY HEALTH</DialogTitle>
+            <DialogContent>
+              <DialogContentText>Please enter your name and email below</DialogContentText>
+              <TextField autoFocus margin="dense" id="name" label="Full Name" type="text" fullWidth />
+              <TextField autoFocus margin="dense" id="email" label="Email Address" type="email" fullWidth />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose} color="primary">
+                Cancel
+              </Button>
+              <Button onClick={handleClose} variant="contained" className={styles.button}>
+                Double My Energy
+              </Button>
+            </DialogActions>
+            <DialogContent>
+              <DialogContentText>
+                We value your privacy and would never spam you. See our privacy policy
+              </DialogContentText>
+            </DialogContent>
+          </Dialog>
         </Grid>
         <Grid item container xs={12}>
-          <Grid item xs={2}></Grid>
+          <Grid item xs={1}></Grid>
           <Grid item xs={1}>
-            <FormatQuoteIcon fontSize="large" />
+            <div className={styles.quotes}>
+              <img className={styles.challengeImage} src={`${process.env.PUBLIC_URL}/assets/quote-l.png`} alt="quote" />
+            </div>
           </Grid>
-          <Grid item xs={6}>
+          <Grid item xs={8}>
             <Typography variant="h4">
               These strategies, tactics, and techniques have been tested and used by thousands of athletes, business
               leaders, and “regular” people with great results.
             </Typography>
           </Grid>
           <Grid item xs={1}>
-            <FormatQuoteIcon fontSize="large" />
-          </Grid>
-          <Grid item xs={2}></Grid>
-        </Grid>
-        <Grid item xs={12} container>
-          <Grid item xs={12} md={6}>
-            <div>
-              <img src={`${process.env.PUBLIC_URL}/assets/wadeBioInfo.png`} alt="logo" />
+            <div className={styles.quotes}>
+              <img className={styles.challengeImage} src={`${process.env.PUBLIC_URL}/assets/quote-r.png`} alt="quote" />
             </div>
           </Grid>
-          <Grid item xs={12} md={6}>
+          <Grid item xs={1}></Grid>
+        </Grid>
+        <Grid item xs={12} container justifyContent="center">
+          <Grid item md={3}></Grid>
+          <Grid item xs={12} md={3}>
+            <div className={styles.wadeImageContain}>
+              <img className={styles.wadeImage} src={`${process.env.PUBLIC_URL}/assets/wadeBioInfo.png`} alt="logo" />
+            </div>
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <Typography variant="h4" style={{ color: "#368B85" }}>
+              Wade Lightheart
+            </Typography>
             <WadeList />
           </Grid>
+          <Grid item md={3}></Grid>
+        </Grid>
+        <Grid item xs={12} className={styles.centerMe}>
+          <Button>
+            <Link to="/privacy">Privacy</Link>
+          </Button>
         </Grid>
       </Grid>
     </div>
--- src/components/challenge/List.js
+++ src/components/challenge/List.js
@@ -5,62 +5,71 @@ import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import CheckCircleIcon from "@material-ui/icons/CheckCircle";
 import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  iconColor: {
+    color: "#368B85",
+  },
+}));
 
 export const ListComponent = () => {
+  const styles = useStyles();
+
   return (
     <>
       <List>
         <ListItem>
           <ListItemIcon>
-            <CheckCircleIcon />
+            <CheckCircleIcon className={styles.iconColor} />
           </ListItemIcon>
           <ListItemText primary="How To Get 70 Percent More Energy In 30 Seconds Or Less" />
         </ListItem>
         <ListItem>
           <ListItemIcon>
-            <CheckCircleIcon />
+            <CheckCircleIcon className={styles.iconColor} />
           </ListItemIcon>
           <ListItemText primary="The Ultimate Key To High Performance, Health And Longevity" />
         </ListItem>
         <ListItem>
           <ListItemIcon>
-            <CheckCircleIcon />
+            <CheckCircleIcon className={styles.iconColor} />
           </ListItemIcon>
           <ListItemText primary="Mastering Your Mind For Health, Wealth And Happiness" />
         </ListItem>
         <ListItem>
           <ListItemIcon>
-            <CheckCircleIcon />
+            <CheckCircleIcon className={styles.iconColor} />
           </ListItemIcon>
           <ListItemText primary="How To Turn The Tide Against Uncontrollable Food Cravings" />
         </ListItem>
         <ListItem>
           <ListItemIcon>
-            <CheckCircleIcon />
+            <CheckCircleIcon className={styles.iconColor} />
           </ListItemIcon>
           <ListItemText primary="How To Select The Most Powerful Supplements For You" />
         </ListItem>
         <ListItem>
           <ListItemIcon>
-            <CheckCircleIcon />
+            <CheckCircleIcon className={styles.iconColor} />
           </ListItemIcon>
           <ListItemText primary="How To Stay Lean And Trim Without Sacrifice" />
         </ListItem>
         <ListItem>
           <ListItemIcon>
-            <CheckCircleIcon />
+            <CheckCircleIcon className={styles.iconColor} />
           </ListItemIcon>
           <ListItemText primary="The Simplest And Fastest Way To Detoxification And Great Skin" />
         </ListItem>
         <ListItem>
           <ListItemIcon>
-            <CheckCircleIcon />
+            <CheckCircleIcon className={styles.iconColor} />
           </ListItemIcon>
           <ListItemText primary="How to optimize Your Brain For Super Intelligence" />
         </ListItem>
         <ListItem>
           <ListItemIcon>
-            <CheckCircleIcon />
+            <CheckCircleIcon className={styles.iconColor} />
           </ListItemIcon>
           <ListItemText primary="And much, much more ..." />
         </ListItem>
